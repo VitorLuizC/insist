991e6a278325890f0c885fe78cdee80925c8aa31
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-06-13 21:41:55 -0300

    🎨 define prettier as code formatter

--- src/insistence.ts
+++ src/insistence.ts
@@ -2,7 +2,7 @@
  * Returns a Promise which is resolved after delay.
  * @param time Delay time in milliseconds.
  */
-function delay (time: number): Promise<any> {
+function delay(time: number): Promise<any> {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
@@ -16,7 +16,7 @@ type InsistΛ = () => boolean | Promise<boolean>;
  * Returns check result or `false` if an error is thrown.
  * @param λ A function that checks if it don't need to keep insisting.
  */
-function getSuccess (λ: InsistΛ): boolean | Promise<boolean> {
+function getSuccess(λ: InsistΛ): boolean | Promise<boolean> {
   try {
     return λ();
   } catch (_) {
@@ -29,19 +29,20 @@ function getSuccess (λ: InsistΛ): boolean | Promise<boolean> {
  * @param λ A function that checks if it don't need to keep insisting.
  * @param time Time in millisencons to insist again.
  */
-function Insistence (λ: InsistΛ, time: number = 200): Promise<void> {
+function Insistence(λ: InsistΛ, time: number = 200): Promise<void> {
   return new Promise((resolve) => {
     const insist = (): Promise<any> => {
       const success = getSuccess(λ);
-      return Promise.resolve(success).then((success) => {
-        if (!success)
-          throw new Error('Success not achieved. Keep insisting.');
-        resolve();
-      })
+      return Promise.resolve(success)
+        .then((success) => {
+          if (!success)
+            throw new Error('Success not achieved. Keep insisting.');
+          resolve();
+        })
         .catch(() => delay(time).then(insist));
     };
     return insist();
   });
 }
 
-export { Insistence as default, InsistΛ }
+export { Insistence as default, InsistΛ };
